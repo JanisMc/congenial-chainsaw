c75e689b34ac4166643e10a1b8021bc11da4deb7
Janis <janismcluckie@googlemail.com>
2021-02-02 17:01:06 +0000

    added event listener to create list item on click

--- 8/app.js
+++ 8/app.js
@@ -1,2 +1,8 @@
 const input = document.querySelector("#input");
 const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", () => {
+    let listItem = document.createElement("li");
+    listItem.textContent = input.value;
+    // console.log(listItem);
+})
