5dfd2ffa8df8cd0b60c58be3aeb47198c9334776
Janis <janismcluckie@googlemail.com>
2021-02-02 17:36:19 +0000

    added show/hide list feature

--- 8/app.js
+++ 8/app.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("#input");
 const submit = document.querySelector("#submit");
 const list = document.querySelector("#list");
+const button = document.querySelector("#showhide-btn");
 
 submit.addEventListener("click", () => {
     let listItem = document.createElement("li");
@@ -8,4 +9,17 @@ submit.addEventListener("click", () => {
     list.appendChild(listItem);
     console.log(listItem);
     input.value = "";
+})
+
+button.addEventListener("click", () => {
+    console.log("button");
+    if (list.style.display === "none") {
+        button.textContent = "hide";
+        list.style.display = "block";
+        // console.log(1);
+    } else {
+        list.style.display = "none";
+        button.textContent = "show";
+        // console.log(2);
+    }
 })
