fd7cd727898726824d689446222db622ced03ecf
Janis <janismcluckie@googlemail.com>
2021-02-02 17:10:58 +0000

    added line to update list with list item on click

--- 8/app.js
+++ 8/app.js
@@ -1,8 +1,10 @@
 const input = document.querySelector("#input");
 const submit = document.querySelector("#submit");
+const list = document.querySelector("#list");
 
 submit.addEventListener("click", () => {
     let listItem = document.createElement("li");
     listItem.textContent = input.value;
-    // console.log(listItem);
+    list.appendChild(listItem);
+    console.log(listItem);
 })
