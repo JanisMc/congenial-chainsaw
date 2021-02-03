ec35fcc8ce8afcd69359f08d0b799a0c5a0913a0
Janis <janismcluckie@googlemail.com>
2021-02-03 12:09:26 +0000

    added for loop for mouseover/mouseout event

---
+++ 11/app.js
@@ -0,0 +1,32 @@
+const input = document.querySelector("#input");
+const button = document.querySelector("#submit");
+const list = document.querySelector("#list");
+const remove = document.querySelector("#remove");
+const listItems = document.querySelectorAll("li")
+
+button.addEventListener("click", () => {
+    let listItem = document.createElement("li");
+    listItem.textContent = input.value;
+    listItem.addEventListener("mouseover", () => {
+        listItem.textContent = listItem.textContent.toUpperCase ();
+    });
+    listItem.addEventListener("mouseout", () => {
+        listItem.textContent = listItem.textContent.toLowerCase ();
+    })
+
+    list.appendChild(listItem);
+    input.value = "";
+})
+
+remove.addEventListener("click", () => {
+    list.removeChild(document.querySelector("#list li:last-child"))
+})
+
+for (let listItem of listItems) {
+    listItem.addEventListener("mouseover", () => {
+        listItem.textContent = listItem.textContent.toUpperCase ();
+    });
+    listItem.addEventListener("mouseout", () => {
+        listItem.textContent = listItem.textContent.toLowerCase ();
+    })
+}
